7cf5e5798df2377a374c7fa9ed7e608ade07abdf
yunixxx <bluefowx@gmail.com>
2019-07-24 17:33:36 +0200

    Inital commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementName = new System.Windows.Forms.TextBox();
+            this.attributeName = new System.Windows.Forms.TextBox();
+            this.attributeValue = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // elementName
+            // 
+            this.elementName.Location = new System.Drawing.Point(44, 36);
+            this.elementName.Name = "elementName";
+            this.elementName.Size = new System.Drawing.Size(100, 20);
+            this.elementName.TabIndex = 1;
+            this.elementName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // attributeName
+            // 
+            this.attributeName.Location = new System.Drawing.Point(195, 36);
+            this.attributeName.Name = "attributeName";
+            this.attributeName.Size = new System.Drawing.Size(100, 20);
+            this.attributeName.TabIndex = 2;
+            this.attributeName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // attributeValue
+            // 
+            this.attributeValue.Location = new System.Drawing.Point(338, 36);
+            this.attributeValue.Name = "attributeValue";
+            this.attributeValue.Size = new System.Drawing.Size(100, 20);
+            this.attributeValue.TabIndex = 3;
+            this.attributeValue.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 46);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 20);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "browse...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "file...";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "pending...";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "pending...";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "pending...";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "element";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(234, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "attribute";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(370, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 460);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.attributeValue);
+            this.Controls.Add(this.attributeName);
+            this.Controls.Add(this.elementName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox elementName;
+        private System.Windows.Forms.TextBox attributeName;
+        private System.Windows.Forms.TextBox attributeValue;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        XDocument doc;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        //specify attribute
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //specify element1
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //specify element1 value
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //button1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(doc == null) {
+                MessageBox.Show("select a file first");
+                return;
+            }
+
+            //if element doesnt exist, go away
+            if(!doc.Root.Elements().Any(v => v.Name == elementName.Text)) {
+                MessageBox.Show("error. invalid element");
+                return;
+            }
+
+            var element = doc.Root.Element(elementName.Text);
+
+            label2.Text = "success!";
+            
+            if(element.Attributes().Any(v => v.Name == attributeName.Text)) {
+                DialogResult result = MessageBox.Show("The attribute already exists. Overwrite it?", "Overwrite?", MessageBoxButtons.YesNo);
+                if(result == DialogResult.Yes)
+                {
+                    var attribute = element.Attribute(attributeName.Text);
+                    attribute.SetValue(attributeValue.Text);
+                }
+                else
+                {
+                    return;
+
+                }
+
+            } else {
+                element.Add(new XAttribute(attributeName.Text, attributeValue.Text));
+            }
+            label3.Text = "success!";
+            // create the attribute
+            
+            label4.Text = "success!";
+
+            doc.Save(openFileDialog1.FileName);
+
+            /*
+            <root>
+                <foo></foo>
+            </root>
+            */
+        }
+
+        //button browse
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK) {
+                doc = XDocument.Load(openFileDialog1.FileName);
+                label1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        //label if good or not 1
+        private void label2_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        //label if good or not 2
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //label if good or not 3
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
